69bb39217358c19ba144836baef6982e9ab0953a
hyvnaond <hyvnaond@fit.cvut.cz>
2021-04-18 13:13:36 +0200

    Add functions to api

--- code/app/api.py
+++ code/app/api.py
@@ -1,6 +1,8 @@
-from flask import Flask, url_for, render_template, request
+from flask import Flask, url_for, render_template, request, jsonify
 from markupsafe import escape
-from . import auth
+from .db import add_address as add_address_db
+from .db import add_alergen as add_allergen_db
+#from . import auth
 
 app = Flask(__name__)
 
@@ -30,6 +32,16 @@ def get_menus():
     return ""
 
 
+@app.route('/menus/', methods=['POST'])
+def add_menu():
+    return ""
+
+
+@app.route('/menus/', methods=['DELETE'])
+def delete_menu():
+    return ""
+
+
 @app.route('/orders', methods=['POST'])
 def add_order():
     return ""
@@ -48,6 +60,30 @@ def add_reservation():
 @app.route('/reservations/<int:id>', methods=['DELETE'])
 def delete_reservation():
     return ""
+
+
+@app.route('/addresses', methods=['POST'])
+def add_address():
+    # TODO read values from form
+    city: str
+    psc: str
+    state: str
+    street: str
+
+    add_address_db(city, psc, state, street)
+
+
+@app.route('allergen/all', methods=['GET'])
+def get_allergens():
+    return ""
+
+
+@app.route('/allergens', method=['POST'])
+def add_allergen():
+    # TODO read value from form
+    name: str
+
+    add_allergen_db(name)
 
 
 @app.route('/auth/register', methods=['GET', 'POST'])
