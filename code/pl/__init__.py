27c46bb9d6587bbcfd5c6a870e070ca8dc391a37
Jiří Szkandera <szkanjir@fit.cvut.cz>
2021-04-18 19:06:45 +0200

    Enable sass and clean up requirements

--- code/pl/__init__.py
+++ code/pl/__init__.py
@@ -1,6 +1,7 @@
 import os
 
 from flask import Flask
+from sassutils.wsgi import SassMiddleware
 
 
 def create_app(test_config=None):
@@ -9,6 +10,10 @@ def create_app(test_config=None):
         SECRET_KEY='dev',
         DATABASE=os.path.join(app.instance_path, 'flaskr.sqlite'),
     )
+
+    app.wsgi_app = SassMiddleware(app.wsgi_app, {
+        __name__: ('static/sass', 'static/css', '/static/css')
+    })
 
     if test_config is None:
         # Load the instance config, if it exists, when not testing
