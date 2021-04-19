736aab506bf396fef9830e5f142df7926618fe69
rencjaku <rencjaku@fit.cvut.cz>
2021-04-19 17:42:31 +0200

    Add coments

--- code/dl/mappers.py
+++ code/dl/mappers.py
@@ -17,18 +17,25 @@ from abc import ABC, abstractstaticmethod
 
 class IDbTableMapper(ABC):
 
+    # [@param-return] All objects from specific table in database
     @abstractstaticmethod
     def get_all():
         pass
 
+    # [@param-in] ID of object we want to get from database
+    # [@param-return] object from specific table
     @abstractstaticmethod
     def get(objID):
         pass
 
+    # [@param-in] new object for database
+    # [@param-return] True at success, False when object is already in database
     @abstractstaticmethod
     def add(obj):
         pass
 
+    # [@param-in] old object for removal
+    # [@param-out] True at success, False when object is not in database
     @abstractstaticmethod
     def delete(obj):
         pass
