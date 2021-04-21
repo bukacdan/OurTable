f1b0d0e8871bf85f7bc88284e2ec59319c61599d
hyvnaond <hyvnaond@fit.cvut.cz>
2021-04-21 17:50:31 +0200

    Fix config file

--- code/config.py
+++ code/config.py
@@ -8,26 +8,26 @@ from dl.mapper.reservation import ReservationMapper
 from dl.mapper.schedule import ScheduleMapper
 from dl.mapper.table import TableMapper
 
-from dl.mapper.iaddress import IAddress
+from dl.mapper.iaddress import IAddressMapper
 from dl.mapper.ialergen import IAlergen
 from dl.mapper.icustomer import ICustomer
-from dl.mapper.imeal import IMeal
-from dl.mapper.imeal_order import IMealOrder
-from dl.mapper.imenu import IMenu
-from dl.mapper.ireservation import IReservation
-from dl.mapper.ischedule import ISchedule
-from dl.mapper.itable import ITable
+from dl.mapper.imeal import IMealMapper
+from dl.mapper.imeal_order import IMealOrderMapper
+from dl.mapper.imenu import IMenuMapper
+from dl.mapper.ireservation import IReservationMapper
+from dl.mapper.ischedule import IScheduleMapper
+from dl.mapper.itable import ITableMapper
 
 from injector import singleton
 
 
 def configure(binder):
-    binder.bind(IAddress, to=AddressMapper, scope=singleton)
-    binder.bind(IAlergen, to=AlergenMapper, scope=singleton)
-    binder.bind(ICustomer, to=CustomerMapper, scope=singleton)
-    binder.bind(IMeal, to=MealMapper, scope=singleton)
-    binder.bind(IMealOrder, to=MealOrderMapper, scope=singleton)
-    binder.bind(IMenu, to=MenuMapper, scope=singleton)
-    binder.bind(IReservation, to=ReservationMapper, scope=singleton)
-    binder.bind(ISchedule, to=ScheduleMapper, scope=singleton)
-    binder.bind(ITable, to=TableMapper, scope=singleton)
+    binder.bind(IAddressMapper, to=AddressMapper, scope=singleton)
+    binder.bind(IAlergenMapper, to=AlergenMapper, scope=singleton)
+    binder.bind(ICustomerMapper, to=CustomerMapper, scope=singleton)
+    binder.bind(IMealMapperMapper, to=MealMapper, scope=singleton)
+    binder.bind(IMealOrderMapper, to=MealOrderMapper, scope=singleton)
+    binder.bind(IMenuMapper, to=MenuMapper, scope=singleton)
+    binder.bind(IReservationMapper, to=ReservationMapper, scope=singleton)
+    binder.bind(IScheduleMapper, to=ScheduleMapper, scope=singleton)
+    binder.bind(ITableMapper, to=TableMapper, scope=singleton)
