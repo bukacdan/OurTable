f0452b9b553b580f28fe843ce4a9ac9e80e1e726
bukacdan <bukacdan@fit.cvut.cz>
2021-04-22 11:48:38 +0200

    add reservation verification

--- code/pl/controller/reservation.py
+++ code/pl/controller/reservation.py
@@ -1,32 +1,35 @@
-from flask import Blueprint, render_template, redirect, url_for, request
+from flask import Blueprint, render_template, redirect, url_for, request, flash
 from .forms import ReservationForm, TableSelectForm
 from dl.mapper.table import TableMapper
 from datetime import datetime
+from werkzeug import exceptions
 
 reservation_bp = Blueprint('reservation_bp', __name__, url_prefix='/reserve')
 
 
-@reservation_bp.route('', methods=['GET'])
+@reservation_bp.route('', methods=['GET', 'POST'])
 def reserve():
     form = ReservationForm()
-    sent_data = []
     if form.validate_on_submit():
-        console.log('here')
-        return redirect(
-            url_for('reservation_bp.select-table', date=form.date.data, time=form.time.data, people=form.guests_cnt.data))
-
-    return render_template('reservation.html', form=form, sent_data=sent_data)
+        return redirect(url_for('reservation_bp.select_table', date=form.date.data, time=form.time.data, guests_cnt=form.guests_cnt.data))
+    return render_template('reservation.html', form=form)
 
 
 @reservation_bp.route('select-table', methods=['GET', 'POST'])
 def select_table():
-    dtime_str = str(request.form['date']) + ' ' + str(request.form['time'])
+    dtime_str = str(request.args['date']) + ' ' + str(request.args['time'])
     dtime = datetime.fromisoformat(dtime_str)
-    free_tables = TableMapper.get_all_free(dtime, request.form['guests_cnt'])
+    free_tables = TableMapper.get_all_free(
+        dtime, request.args['guests_cnt'])
     form = TableSelectForm(tables=free_tables)
+    if form.validate_on_submit():
+        return redirect(url_for('reservation_bp.finish', table_id=request.form['table_id'], dtime=dtime))
     return render_template('table_select.html', tables=free_tables, form=form)
-
+    
 
 @reservation_bp.route('finish', methods=['GET', 'POST'])
 def finish():
-    pass
+    dtime = datetime.fromisoformat(request.args['dtime'])
+    table_id = request.args['table_id']
+    success = False
+    return render_template('reserve_final.html', dtime=dtime, table_id=table_id, success=success)
