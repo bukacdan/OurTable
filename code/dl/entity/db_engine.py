d3107f23bd8f29d4846ae9ef390f889d435eafde
Jiří Szkandera <szkanjir@fit.cvut.cz>
2021-04-25 11:25:10 +0200

    Add commentary to db_engine

--- code/dl/entity/db_engine.py
+++ code/dl/entity/db_engine.py
@@ -4,7 +4,12 @@ from sqlalchemy.ext.automap import automap_base
 
 
 class DBEngine():
-    db_string = 'postgresql://postgres:postgres@localhost:5432/our_table'
+    '''
+    Class encapsulating SQLAlchemy database engine
+
+    This class is a singleton.
+    '''
+    db_destination = 'postgresql://postgres:postgres@localhost:5432/our_table'
     engine = None
     session = None
     base = None
@@ -12,39 +17,46 @@ class DBEngine():
 
     @classmethod
     def get_engine(cls):
+        '''Retrieve database engine'''
         if not cls.valid:
             cls.__init()
         return cls.engine
 
     @classmethod
     def get_session(cls):
+        '''Retrieve database session'''
         if not cls.valid:
             cls.__init()
         return cls.session
 
     @classmethod
     def get_base(cls):
+        '''Retrieve database base object'''
         if not cls.valid:
             cls.__init()
         return cls.base
 
     @classmethod
     def __init(cls):
+        '''Initialize singleton'''
         cls.__init_engine()
         cls.__init_session()
         cls.__init_base()
 
     @classmethod
     def __init_engine(cls):
-        cls.engine = create_engine(cls.db_string)
+        '''Initialize database engine'''
+        cls.engine = create_engine(cls.db_destination)
         cls.connection = cls.engine.connect()
 
     @classmethod
     def __init_session(cls):
+        '''Initialize database session'''
         Session = sessionmaker(bind=cls.engine)
         cls.session = Session()
 
     @classmethod
     def __init_base(cls):
+        '''Initialize database base object'''
         cls.base = automap_base(bind=cls.engine)
         cls.base.prepare(cls.engine, reflect=True)
